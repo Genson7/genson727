b0cbc366deab918f740fe1dc7f8c39ce4e8ebe04
Genson7 <genson726@proton.me>
2026-06-15 13:47:30 +0000

    added javascript

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-const burger = document.querySelector(".burger");
-const navMenu = document.querySelector(".nav-menu");
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
 
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
+  //TOGGLE NAV
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
 
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click ", () => {
-    burger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+
+  //ANIMATE LINKS
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+    }
+  });
+  //BURGER ANIMATION
+  burger.classList.toggle('toggle');
+  });
+}
+
+navSlide();
